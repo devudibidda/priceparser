bf835ce9384982f9246618ee847d14f0d7d1b33c
Nanaji Guntreddi <nandankrishnag@outlook.com>
2024-07-13 21:30:41 +0530

    Update util.py
    added parquet

--- util.py
+++ util.py
@@ -1,18 +1,28 @@
 import os
-
 import dask.dataframe as dd
 
 # Directory containing CSV files
 csv_directory = 'product_feeds'
+parquet_directory = 'product_feeds_parquet'
+
+# Convert CSV files to Parquet
+if not os.path.exists(parquet_directory):
+    os.makedirs(parquet_directory)
+
+for entry in os.scandir(csv_directory):
+    if entry.name.endswith('.csv') and entry.is_file():
+        df = dd.read_csv(entry.path, dtype={'discount': 'float64'})
+        parquet_path = os.path.join(parquet_directory, os.path.splitext(entry.name)[0] + '.parquet')
+        df.to_parquet(parquet_path, engine='pyarrow')
 
 # List to store computations
 computations = []
 
-# Iterate through CSV files in the directory
-for entry in os.scandir(csv_directory):
-    if entry.name.endswith('.csv') and entry.is_file():
+# Iterate through Parquet files in the directory
+for entry in os.scandir(parquet_directory):
+    if entry.name.endswith('.parquet') and entry.is_file():
         category = os.path.splitext(entry.name)[0]  # Extract category from filename
-        df = dd.read_csv(entry.path, dtype={'discount': 'float64'})
+        df = dd.read_parquet(entry.path, engine='pyarrow')
         df['discount_percentage'] = ((df['mrp'] - df['sellingPrice']) / df['mrp']) * 100
         discounted_products = df[df['discount_percentage'] > 30]
         computations.append((category, discounted_products))
